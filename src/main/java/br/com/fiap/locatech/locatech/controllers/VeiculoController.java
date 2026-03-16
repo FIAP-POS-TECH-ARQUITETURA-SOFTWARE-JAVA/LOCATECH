a25d2ecab4394ff9eff788f87509329297fc5255
DeivisDigiHub <161029225+DeivisDigiHub@users.noreply.github.com>
2026-03-16 13:39:00 -0300

    feat: subindo o projeto

---
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/VeiculoController.java
@@ -0,0 +1,18 @@
+package br.com.fiap.locatech.locatech.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/veiculos" )
+
+public class VeiculoController {
+
+    @GetMapping
+    public String getVeiculos() {
+        return "Veiculo Listagem";
+
+
+    }
+}
